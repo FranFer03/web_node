382d563d19610129436849db9235ca9d6772b476
Fernandez Francisco <Franfer0301@gmail.com>
2025-05-03 23:14:37 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,10 +1,15 @@
-const API_URL = "https://ht5itniv36.execute-api.sa-east-1.amazonaws.com/meshlora/users"; // Cambia esto por tu API real
+const API_URL = "https://ht5itniv36.execute-api.sa-east-1.amazonaws.com/meshlora";
+
+if (window.location.pathname !== "/login.html" &&
+  localStorage.getItem("autenticado") !== "true") {
+window.location.href = "login.html";
+}
 
 function iniciarSesion() {
   const usuario = document.getElementById("usuario").value;
   const contrasena = document.getElementById("contrasena").value;
 
-  fetch(`${API_URL}/login`, {
+  fetch(`${API_URL}/user`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ usuario, contrasena })
@@ -13,6 +18,7 @@ function iniciarSesion() {
     .then(data => {
       if (data.success) {
         localStorage.setItem("autenticado", "true");
+        localStorage.setItem("token", data.token);  // (opcional)
         window.location.href = "index.html";
       } else {
         document.getElementById("mensaje-error").innerText = "Credenciales inválidas";
